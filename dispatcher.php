3bde2e1a627b1c2666b56e2d5927da2d210b069d
Andrew Thomas <poproar@users.noreply.github.com>
2014-06-30 10:49:31 -0700

    Create dispatcher.php

---
+++ dispatcher.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * @package bg
+ * @copyright Copyright (c)2014 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @license GNU General Public License version 2 or later
+ */
+
+defined('_JEXEC') or die();
+
+class TodoDispatcher extends FOFDispatcher
+{
+	public $defaultView = 'profiles';
+
+	public function onBeforeDispatch()
+	{
+		$result = parent::onBeforeDispatch();
+
+		if ($result)
+		{
+			$strapperInclude = JPATH_ROOT . '/media/akeeba_strapper/strapper.php';
+
+			if (@file_exists($strapperInclude))
+			{
+				@include_once $strapperInclude;
+			}
+
+			if (class_exists('AkeebaStrapper'))
+			{
+				AkeebaStrapper::bootstrap();
+				AkeebaStrapper::jQueryUI();
+			}
+		}
+
+		return $result;
+	}
+}
