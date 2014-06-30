f5b7f567de3d71059b3bf91fa43c20cb0182fd5a
Andrew Thomas <poproar@users.noreply.github.com>
2014-06-30 12:24:34 -0700

    update profile model to current
    as on ubuntu dev box

--- models/profiles.php
+++ models/profiles.php
@@ -12,7 +12,37 @@ class BgModelProfiles extends FOFModel
 {
 	public function buildQuery($overrideLimits = false)
 	{
+		$db = $this->getDbo();
+		$query = parent::buildQuery($overrideLimits = false);
+        	//$this->setState('filter_order', 'bg_day_id');
+
+        	$query->where($db->qn('cb_status').'  is not null'); // new versus active? featured?
+
+
+        	$week = $this->getState('weekSearch', null);
+
+        	// this doesnt work on sat or sunday  so....
+       
+        	// something bigger is not working
+        	if($week == 'thisWeek')
+        	{
+        	    $query->where('WEEKOFYEAR('. $db->qn('cb_next'). ') = WEEKOFYEAR(CURDATE())');
+        	}
+        	elseif($week == 'nextWeek')
+        	{
+        	    $query->where('WEEKOFYEAR('. $db->qn('cb_next'). ') = (WEEKOFYEAR(CURDATE()) + 1) ');
+        	}	
+        	elseif($week == 'lastWeek')
+        	{
+        	    $query->where('WEEKOFYEAR('. $db->qn('cb_last'). ') = (WEEKOFYEAR(CURDATE()) - 1)');
+        	}
+
+        	if($this->getState('isNew', null))
+        	{
+        	    $query->where($db->qn('cb_last'). ' is null OR ' .  $db->qn('cb_last') . '= ' .$db->q('0000-00-00') . ' OR ' .  $db->qn('cb_status') . ' = 1');
+        	}
 		
-		// return $query;
+		return $query;
+		
 	}
 }
