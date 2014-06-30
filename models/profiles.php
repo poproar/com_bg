d0b5f2c09b47fc775632ef1372c37bcfe0b9cb8c
Andrew Thomas <poproar@users.noreply.github.com>
2014-06-30 11:22:31 -0700

    Create profiles.php

---
+++ models/profiles.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package bg
+ * @copyright Copyright (c)2010-2014 Nicholas K. Dionysopoulos
+ * @license GNU General Public License version 3, or later
+ */
+
+// Protect from unauthorized access
+defined('_JEXEC') or die();
+
+class BgModelProfiles extends FOFModel
+{
+	public function buildQuery($overrideLimits = false)
+	{
+		
+		// return $query;
+	}
+}
