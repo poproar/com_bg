4013ac9a11cef4996b18e24c0c9e3ed7843abc46
Andrew Thomas <git-user@poproar.com>
2014-06-30 22:32:25 -0700

    real information displayed

--- views/profile/tmpl/form.php
+++ views/profile/tmpl/form.php
@@ -1,8 +1,12 @@
 <?php
-
+dump($this, 'this');
 // Show some stuff before the form
-?><h1>Hello, world!</h1>
-<p>This is printed above the form</p>
+?><h1><?php echo $this->item->firstname . " " . $this->item->lastname; ?></h1>
+<address>
+	<?php echo $this->item->address . " " . $this->item->cb_street2; ?><br/>
+	<?php echo $this->item->city . ", CA " . $this->item->zipcode; ?><br/>
+	<?php echo $this->item->phone; ?>
+</address>
 
 <?php
 // Show the rendered form
@@ -10,4 +14,4 @@ echo $this->getRenderedForm();
 
 // Show some stuff after the form
 ?>
-<p>This is printed below the form</p>
+<small class="alert alert-warning"	>To edit the address and adjust last date delivery please use the community builder user manager.</small>
