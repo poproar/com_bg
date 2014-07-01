8c0b868f748d6604e2910de134fb0457a490e504
Andrew Thomas <git-user@poproar.com>
2014-06-30 22:59:08 -0700

    move address section

--- views/profile/tmpl/form.php
+++ views/profile/tmpl/form.php
@@ -1,12 +1,4 @@
-<?php
-dump($this, 'this');
-// Show some stuff before the form
-?><h1><?php echo $this->item->firstname . " " . $this->item->lastname; ?></h1>
-<address>
-	<?php echo $this->item->address . " " . $this->item->cb_street2; ?><br/>
-	<?php echo $this->item->city . ", CA " . $this->item->zipcode; ?><br/>
-	<?php echo $this->item->phone; ?>
-</address>
+<h1><?php echo $this->item->firstname . " " . $this->item->lastname; ?></h1>
 
 <?php
 // Show the rendered form
@@ -14,4 +6,12 @@ echo $this->getRenderedForm();
 
 // Show some stuff after the form
 ?>
+<div class="span4">
+<address>
+	<?php echo $this->item->address . " " . $this->item->cb_street2; ?><br/>
+	<?php echo $this->item->city . ", CA " . $this->item->zipcode; ?><br/>
+	<?php echo $this->item->phone; ?>
+</address>
 <small class="alert alert-warning"	>To edit the address and adjust last date delivery please use the community builder user manager.</small>
+
+</div>
